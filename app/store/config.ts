1240bfa048c1efd3fbc897f2b80fb59f37ecdc86
jimao <shihao.gu@perfma.com>
2025-02-06 09:27:49 +0800

    feat: phi4:latest first

--- app/store/config.ts
+++ app/store/config.ts
@@ -60,7 +60,7 @@ export const DEFAULT_CONFIG = {
   dontShowMaskSplashScreen: false, // dont show splash screen when create chat
   hideBuiltinMasks: false, // dont add builtin masks
 
-  customModels: "deepseek-r1:14b,deepseek-r1:32b,phi4:latest",
+  customModels: "phi4:latest,deepseek-r1:14b,deepseek-r1:32b",
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
