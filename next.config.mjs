d230ae037833f1140ebaf92bd346f1231885a76b
jimao <shihao.gu@perfma.com>
2025-02-06 16:41:14 +0800

    feat: openrouter proxy

--- next.config.mjs
+++ next.config.mjs
@@ -100,6 +100,10 @@ if (mode !== "export") {
         source: "/api/proxy/alibaba/:path*",
         destination: "https://dashscope.aliyuncs.com/api/:path*",
       },
+      {
+        source: "/api/openai/:path*",
+        destination: "https://openrouter.ai/api/:path*",
+      },
     ];
 
     return {
