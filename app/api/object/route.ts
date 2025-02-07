e4025a7e91dfa1667b69c037ee049ebba9042f70
jimao <shihao.gu@perfma.com>
2025-02-07 23:30:26 +0800

    feat: 目标分页验证通过

--- app/api/object/route.ts
+++ app/api/object/route.ts
@@ -4,7 +4,7 @@ import XSeaSimplifier from "../simplifier/xseaSimplifier";
 export async function GET() {
   try {
     const xsea = new XSeaSimplifier();
-    return NextResponse.json(await xsea.PlanPaging("915184367784673280"), {
+    return NextResponse.json(await xsea.GoalPaging("841402405221584896"), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
