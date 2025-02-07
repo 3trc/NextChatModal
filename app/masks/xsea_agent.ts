bf6e48a0884a3d9c65f585361fe150dc85fc6614
jimao <shihao.gu@perfma.com>
2025-02-07 18:52:38 +0800

    feat: 知识库prompt完善

--- app/masks/xsea_agent.ts
+++ app/masks/xsea_agent.ts
@@ -1,3 +1,4 @@
+import { XSEA_Knowledge } from "./knowledge/xsea";
 import { BuiltinMask } from "./typing";
 
 export const XSEA_AGENTS: BuiltinMask[] = [
@@ -7,8 +8,22 @@ export const XSEA_AGENTS: BuiltinMask[] = [
     context: [
       {
         id: "",
-        role: "user",
-        content: "你好",
+        role: "system",
+        content:
+          "XSea性能测试平台是一个强大的用于性能测试的软件系统，你是准确了解XSea性能测试平台各种知识的AI助手。接下来我会发送给你XSea性能测试平台的相关文档，你需要以此为基础回答用户操作流程，知识概念，常见问题等等方面的问题。",
+        date: "",
+      },
+      {
+        id: "",
+        role: "system",
+        content: XSEA_Knowledge,
+        date: "",
+      },
+      {
+        id: "",
+        role: "system",
+        content:
+          "对于你不会的问题，你就说不知道。对于用户表达含糊或者你不是很确定的问题，请寻求澄清。回答问题一定要结合上述产品知识库以及压力测试的行业技术背景知识。聊天中一定要避免透露你是在引用知识库文档，你需要像是一个真的助手一样回答问题。对于非XSea性能测试平台或者非测试相关的问题，请一定不要回答。",
         date: "",
       },
     ],
