b3afe1642e1765d44e64256bc9f9fb228122ad62
jimao <shihao.gu@perfma.com>
2025-02-07 17:14:39 +0800

    feat: MASS_MODELS使用

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -1,22 +1,8 @@
-import { CUSTOM_MODEL_LIST } from "../constant";
+import { MASS_MODELS } from "./maas_model";
 import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
-  ...CUSTOM_MODEL_LIST.map(
-    (model) =>
-      ({
-        avatar: "1f916",
-        name: model.split("/")[1],
-        context: [],
-        modelConfig: {
-          model: model,
-          max_tokens: 20000,
-        },
-        lang: "cn",
-        builtin: true,
-        createdAt: 1688899480511,
-      }) as BuiltinMask,
-  ),
+  ...MASS_MODELS,
   // {
   //   avatar: "1f638",
   //   name: "文案写手",
