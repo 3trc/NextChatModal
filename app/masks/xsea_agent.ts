ab85ecece47ce4af994493ab7976312b409cb82d
jimao <shihao.gu@perfma.com>
2025-02-07 18:13:06 +0800

    feat: 多个agent

--- app/masks/xsea_agent.ts
+++ app/masks/xsea_agent.ts
@@ -3,7 +3,140 @@ import { BuiltinMask } from "./typing";
 export const XSEA_AGENTS: BuiltinMask[] = [
   {
     avatar: "1f4da",
-    name: "XSea知识库",
+    name: "XSea-知识库",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "1f4da",
+    name: "压测-Agent",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "2699",
+    name: "创建脚本-Agent",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "2699",
+    name: "创建目标-Agent",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "2699",
+    name: "产品列表-Agent",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "2699",
+    name: "计划列表-Agent",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "2699",
+    name: "脚本列表-Agent",
+    context: [
+      {
+        id: "",
+        role: "user",
+        content: "你好",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "mistralai/mistral-small-24b-instruct-2501",
+      max_tokens: 32768,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "2699",
+    name: "目标列表-Agent",
     context: [
       {
         id: "",
