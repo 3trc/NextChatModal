b29395d618dd989178ddb2884252f648c4ff7e12
jimao <shihao.gu@perfma.com>
2025-02-08 11:14:36 +0800

    feat: 产品分页接口

--- app/api/object/xsea/product/route.ts
+++ app/api/object/xsea/product/route.ts
@@ -1,13 +1,29 @@
 import XSeaSimplifier from "@/app/api/simplifier/xseaSimplifier";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+function pagingParams(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const search = searchParams.has("search") ? searchParams.get("search")! : "";
+  const pageNum = searchParams.has("pageNum")
+    ? Number(searchParams.get("pageNum"))
+    : 1;
+  const pageSize = searchParams.has("pageSize")
+    ? Number(searchParams.get("pageSize"))
+    : 10;
+  return { search, pageNum, pageSize };
+}
+
+export async function GET(request: NextRequest) {
   try {
+    const params = pagingParams(request);
     const xsea = new XSeaSimplifier();
-    return NextResponse.json(await xsea.ProductPaging(), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json(
+      await xsea.ProductPaging(params.pageNum, params.pageSize, params.search),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
   } catch (error) {
     return NextResponse.json(
       {
