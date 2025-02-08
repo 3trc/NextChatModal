748bf7487e82c5355be52a349849d8fa1e5b2ef0
jimao <shihao.gu@perfma.com>
2025-02-08 11:43:13 +0800

    feat: 产品分页改造

--- app/api/object/xsea/product/route.ts
+++ app/api/object/xsea/product/route.ts
@@ -1,36 +1,11 @@
+import { pagingFactory } from "@/app/api/object/paging";
 import XSeaSimplifier from "@/app/api/simplifier/xseaSimplifier";
-import { NextRequest, NextResponse } from "next/server";
 
-function pagingParams(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const search = searchParams.has("search") ? searchParams.get("search")! : "";
-  const pageNum = searchParams.has("pageNum")
-    ? Number(searchParams.get("pageNum"))
-    : 1;
-  const pageSize = searchParams.has("pageSize")
-    ? Number(searchParams.get("pageSize"))
-    : 10;
-  return { search, pageNum, pageSize };
-}
-
-export async function GET(request: NextRequest) {
-  try {
-    const params = pagingParams(request);
-    const xsea = new XSeaSimplifier();
-    return NextResponse.json(
-      await xsea.ProductPaging(params.pageNum, params.pageSize, params.search),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-  } catch (error) {
-    return NextResponse.json(
-      {
-        code: 500,
-        message: "Internal Server Error",
-      },
-      { status: 500 },
-    );
-  }
-}
+export const GET = pagingFactory(async (params, searchParams) => {
+  const xsea = new XSeaSimplifier();
+  return await xsea.ProductPaging(
+    searchParams.pageNum,
+    searchParams.pageSize,
+    searchParams.search,
+  );
+});
