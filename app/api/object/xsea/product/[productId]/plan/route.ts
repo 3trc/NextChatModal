a52c08195ddfe6d99c460cfd8008f8f38b6459dd
jimao <shihao.gu@perfma.com>
2025-02-08 11:16:27 +0800

    feat: 计划分页接口

---
+++ app/api/object/xsea/product/[productId]/plan/route.ts
@@ -0,0 +1,41 @@
+import XSeaSimplifier from "@/app/api/simplifier/xseaSimplifier";
+import { NextRequest, NextResponse } from "next/server";
+
+function pagingParams(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const search = searchParams.has("search") ? searchParams.get("search")! : "";
+  const pageNum = searchParams.has("pageNum")
+    ? Number(searchParams.get("pageNum"))
+    : 1;
+  const pageSize = searchParams.has("pageSize")
+    ? Number(searchParams.get("pageSize"))
+    : 10;
+  return { search, pageNum, pageSize };
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const params = pagingParams(request);
+    const xsea = new XSeaSimplifier();
+    return NextResponse.json(
+      await xsea.PlanPaging(
+        "822351567109226496",
+        params.pageNum,
+        params.pageSize,
+        params.search,
+      ),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      { status: 500 },
+    );
+  }
+}
