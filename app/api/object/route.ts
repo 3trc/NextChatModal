7fed8c6394fe25931ffd0e7e6546af281a14e87f
jimao <shihao.gu@perfma.com>
2025-02-08 12:42:38 +0800

    feat: 目标创建通过

--- app/api/object/route.ts
+++ app/api/object/route.ts
@@ -5,10 +5,11 @@ export async function GET() {
   try {
     const xsea = new XSeaSimplifier();
     return NextResponse.json(
-      await xsea.PlanCreate(
-        "977785836752531456",
-        "鸡毛聚合接口计划",
-        "简单测试一下",
+      await xsea.GoalCreate(
+        "852524668473987072",
+        "鸡毛聚合目标",
+        "SINGLE_USER_TREND",
+        ["852522201942216704"],
       ),
       {
         status: 200,
