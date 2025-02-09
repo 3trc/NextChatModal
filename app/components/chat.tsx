344cc174e70b815475202cc23d724651550ed065
jimao <shihao.gu@perfma.com>
2025-02-09 10:28:10 +0800

    feat: 不填充你好

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1342,7 +1342,7 @@ function _Chat() {
     // if (!accessStore.isAuthorized()) {
     //   copiedHello.content = Locale.Error.Unauthorized;
     // }
-    context.push(copiedHello);
+    // context.push(copiedHello);
   }
 
   // preview messages
