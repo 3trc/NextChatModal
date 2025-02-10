1779c0c7080e637fe29faca37366bf3d265b14a3
jimao <shihao.gu@perfma.com>
2025-02-10 16:39:58 +0800

    feat: api的keepAlive

--- app/api/agent/xsea/router/route.ts
+++ app/api/agent/xsea/router/route.ts
@@ -5,6 +5,7 @@ const model = new ChatOllama({
   baseUrl: "http://183.220.36.102:32141",
   model: "phi4:latest",
   numCtx: 16384,
+  keepAlive: "10000h",
 });
 
 export async function GET(request: NextRequest, { params }: { params: any }) {
