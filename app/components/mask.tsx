35196789aad788a9654fc22903d71518d8e4d336
jimao <shihao.gu@perfma.com>
2025-02-11 19:12:34 +0800

    feat: name

--- app/components/mask.tsx
+++ app/components/mask.tsx
@@ -601,7 +601,7 @@ export function MaskPage() {
                     icon={<AddIcon />}
                     text={Locale.Mask.Item.Chat}
                     onClick={() => {
-                      chatStore.newSessionX(m.name, "FULL");
+                      chatStore.newSessionX(m.name, "NAME");
                       navigate(Path.Chat);
                     }}
                   />
