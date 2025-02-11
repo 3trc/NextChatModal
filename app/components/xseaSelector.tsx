186e5a738f7303f165317eee20667526af1c3f86
jimao <shihao.gu@perfma.com>
2025-02-11 16:22:50 +0800

    feat: 选择器基本完成

--- app/components/xseaSelector.tsx
+++ app/components/xseaSelector.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import styles from "./xseaSelector.module.scss";
-import { Input, Table } from "antd";
+import { Button, Input, Table } from "antd";
 import axios, { AxiosResponse } from "axios";
 
 const workspaceId = "849903850940473344";
@@ -21,7 +21,7 @@ const LABELS_MAP = {
 const XSeaSelector = (props: { message: string }) => {
   const label = props.message.trim();
   const [loading, setLoading] = useState<boolean>(true);
-  const [filter, setFilter] = useState({ name: "", pageNum: 1, pageSize: 5 });
+  const [filter, setFilter] = useState({ search: "", pageNum: 1, pageSize: 5 });
   const [page, setPage] = useState({
     pageNum: 1,
     pageSize: 5,
@@ -104,12 +104,12 @@ const XSeaSelector = (props: { message: string }) => {
         <span>以下是现有可供选择的{LABELS_MAP[label]}列表：</span>
       </div>
       <div className={styles.title}>
-        <span></span>
+        <span>{LABELS_MAP[label]}</span>
         <Input.Search
           className={styles.searcher}
           size="middle"
           placeholder="请输入任意关键词搜索"
-          onSearch={(value) => updatePage({ name: value, pageNum: 1 })}
+          onSearch={(value) => updatePage({ search: value, pageNum: 1 })}
         />
       </div>
       <div>
@@ -129,12 +129,25 @@ const XSeaSelector = (props: { message: string }) => {
             type: "checkbox",
           }}
           pagination={{
+            position: ["bottomLeft"],
             total: page.total ?? 0,
             current: page.pageNum ?? 1,
             pageSize: page.pageSize ?? 5,
             onChange: (pageNum, pageSize) => updatePage({ pageNum, pageSize }),
           }}
         />
+      </div>
+      <div className={styles.bottom}>
+        <Button
+          className={styles.confirm_button}
+          size="small"
+          type="primary"
+          onClick={() => {
+            alert("写入session状态");
+          }}
+        >
+          选定
+        </Button>
       </div>
     </div>
   );
