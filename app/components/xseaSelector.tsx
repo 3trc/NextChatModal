94eea974f0958f5ba53ab806add0f024e440762d
jimao <shihao.gu@perfma.com>
2025-02-11 14:54:44 +0800

    feat: 选择器

--- app/components/xseaSelector.tsx
+++ app/components/xseaSelector.tsx
@@ -1,19 +1,49 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./xseaSelector.module.scss";
 import { Table } from "antd";
 
+const LABELS_MAP = {
+  "<ui-products>": "产品",
+  "<ui-jmeter-scripts>": "JMeter脚本",
+  "<ui-gatling-scripts>": "Gatling脚本",
+  "<ui-shell-scripts>": "Shell脚本",
+  "<ui-plans>": "计划",
+  "<ui-goals>": "目标",
+  "<ui-records>": "记录",
+} as any;
+
 const XSeaSelector = (props: { message: string }) => {
+  const label = props.message.trim();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [filter, setFilter] = useState({ name: "", pageNum: 1, pageSize: 10 });
+
+  const updatePage = async (params: any = {}) => {
+    const newFilter = { ...filter, ...params };
+    setFilter(newFilter);
+    setLoading(true);
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    updatePage();
+  }, []);
+
   return (
     <div className={styles.com}>
       <div className={styles.introduction}>
-        <span>以下是脚本列表</span>
+        <span>以下是现有的{LABELS_MAP[label]}列表</span>
       </div>
       <div>
         <Table
           size="small"
           bordered
+          loading={loading}
           columns={[
             {
               title: "名称",
@@ -28,6 +58,9 @@ const XSeaSelector = (props: { message: string }) => {
               name: "脚本2",
             },
           ]}
+          rowSelection={{
+            type: "checkbox",
+          }}
         />
       </div>
     </div>
