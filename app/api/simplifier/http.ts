dabc3316b706f86baab170ddfc0c5b7258fb5216
jimao <shihao.gu@perfma.com>
2025-02-11 15:23:20 +0800

    feat: 除了脚本之外的选择器

--- app/api/simplifier/http.ts
+++ app/api/simplifier/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL: "http://10.10.30.103:8081/api",
   headers: {
-    cookie: "sys_token=d06370ee777f44a3889c81ab4c4321a8",
+    cookie: "sys_token=5eb97f2fdc9040e39e53232d10562cfb",
   },
 });
 
--- app/components/xseaSelector.tsx
+++ app/components/xseaSelector.tsx
@@ -3,6 +3,10 @@
 import React, { useEffect, useState } from "react";
 import styles from "./xseaSelector.module.scss";
 import { Table } from "antd";
+import axios, { AxiosResponse } from "axios";
+
+const workspaceId = "849903850940473344";
+const planId = "841402405221584896";
 
 const LABELS_MAP = {
   "<ui-products>": "产品",
@@ -17,13 +21,46 @@ const LABELS_MAP = {
 const XSeaSelector = (props: { message: string }) => {
   const label = props.message.trim();
   const [loading, setLoading] = useState<boolean>(true);
-  const [filter, setFilter] = useState({ name: "", pageNum: 1, pageSize: 10 });
+  const [filter, setFilter] = useState({ name: "", pageNum: 1, pageSize: 5 });
+  const [page, setPage] = useState({
+    pageNum: 1,
+    pageSize: 5,
+    list: [],
+    total: 0,
+  });
 
   const updatePage = async (params: any = {}) => {
     const newFilter = { ...filter, ...params };
     setFilter(newFilter);
     setLoading(true);
     try {
+      let res: AxiosResponse<any, any>;
+      if (label === "<ui-products>") {
+        res = await axios.get("/api/object/xsea/product", {
+          params: newFilter,
+        });
+        setPage(res.data);
+      }
+      if (label === "<ui-plans>") {
+        res = await axios.get(`/api/object/xsea/product/${workspaceId}/plan`, {
+          params: newFilter,
+        });
+        setPage(res.data);
+      }
+      if (label === "<ui-goals>") {
+        res = await axios.get(
+          `/api/object/xsea/product/${workspaceId}/plan/${planId}/goal`,
+          { params: newFilter },
+        );
+        setPage(res.data);
+      }
+      if (label === "<ui-records>") {
+        res = await axios.get(
+          `/api/object/xsea/product/${workspaceId}/plan/${planId}/test-record`,
+          { params: newFilter },
+        );
+        setPage(res.data);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -32,15 +69,16 @@ const XSeaSelector = (props: { message: string }) => {
 
   useEffect(() => {
     updatePage();
-  }, []);
+  }, [props.message]);
 
   return (
     <div className={styles.com}>
       <div className={styles.introduction}>
-        <span>以下是现有的{LABELS_MAP[label]}列表</span>
+        <span>以下是现有可供选择的{LABELS_MAP[label]}列表：</span>
       </div>
       <div>
         <Table
+          rowKey="id"
           size="small"
           bordered
           loading={loading}
@@ -50,16 +88,15 @@ const XSeaSelector = (props: { message: string }) => {
               dataIndex: "name",
             },
           ]}
-          dataSource={[
-            {
-              name: "脚本1",
-            },
-            {
-              name: "脚本2",
-            },
-          ]}
+          dataSource={page.list ?? []}
           rowSelection={{
             type: "checkbox",
+          }}
+          pagination={{
+            total: page.total ?? 0,
+            current: page.pageNum ?? 1,
+            pageSize: page.pageSize ?? 5,
+            onChange: (pageNum, pageSize) => updatePage({ pageNum, pageSize }),
           }}
         />
       </div>
