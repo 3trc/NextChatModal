703ca965a53d9c6a4ddf904846849378a98a1141
jimao <shihao.gu@perfma.com>
2025-02-12 10:40:30 +0800

    feat: 这一块流程需要修改

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1134,8 +1134,6 @@ function _Chat() {
             chatStore.newSessionX(`XSea-${hookResult.intention}`, "NONE");
             navigate(Path.Chat);
             chatStore.onUserInput(hookResult.userContent);
-            setSendButtonLoading(false);
-            return;
           }
         } catch (error) {
           setSendButtonLoading(false);
