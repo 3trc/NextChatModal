f667de6c81c97052f81ad3894638aa7eb3dc831c
jimao <shihao.gu@perfma.com>
2025-02-14 14:12:13 +0800

    feat: 读取的undefined

--- app/components/xsea/localJSON.ts
+++ app/components/xsea/localJSON.ts
@@ -1,6 +1,10 @@
 const handler = {
   get: (_: any, property: any) => {
-    return JSON.parse(localStorage.getItem(property) as string);
+    try {
+      return JSON.parse(localStorage.getItem(property) as string);
+    } catch (error) {
+      return undefined;
+    }
   },
   set: (_: any, property: any, value: any) => {
     localStorage.setItem(property, JSON.stringify(value));
