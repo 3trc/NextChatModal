c5f968bfc63bde22a5c0b5865bb1905d70ad1fa8
jimao <shihao.gu@perfma.com>
2025-02-14 17:07:56 +0800

    feat: 展开收起

--- app/components/statesView/index.tsx
+++ app/components/statesView/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./index.module.scss";
+import { Button } from "antd";
 
 const StatesView = () => {
-  return <div>状态视图</div>;
+  const [expand, setExpand] = useState<boolean>(true);
+
+  return (
+    <div className={styles.com} style={{ width: expand ? "300px" : "100px" }}>
+      <Button type="primary" onClick={() => setExpand(() => !expand)}>
+        {expand ? "收起" : "展开"}
+      </Button>
+    </div>
+  );
 };
 
 export default StatesView;
