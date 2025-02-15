527128342111075d9d97101bb1952bbfa56f1e20
jimao <shihao.gu@perfma.com>
2025-02-15 15:13:58 +0800

    feat: 重新更换端口

--- app/api/agent/xsea/model.ts
+++ app/api/agent/xsea/model.ts
@@ -1,7 +1,7 @@
 import { ChatOllama } from "@langchain/ollama";
 
 const model = new ChatOllama({
-  baseUrl: "http://183.220.36.102:31901",
+  baseUrl: "http://183.220.36.102:31811",
   model: "phi4:latest",
   numCtx: 16384,
   keepAlive: "10000h",
