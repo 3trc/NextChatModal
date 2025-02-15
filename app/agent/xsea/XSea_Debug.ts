2ccda8c43eef9df6c44cb96194dda78e16033246
jimao <shihao.gu@perfma.com>
2025-02-15 22:55:32 +0800

    feat: 测试Agent

---
+++ app/agent/xsea/XSea_Debug.ts
@@ -0,0 +1,23 @@
+import { AgentLifeCycle } from "..";
+
+export default {
+  avatar: "🔄",
+  name: "XSea_Debug",
+  context: [
+    {
+      id: "",
+      role: "system",
+      content: `
+确保中文回答，确保回答"你好"，避免回答非"你好"文本，避免回答超过两个汉字，避免回答英文
+      `.trim(),
+      date: "",
+    },
+  ],
+  modelConfig: {
+    model: "perfma-gpt-14b:latest",
+    max_tokens: 16384,
+    topK: 1,
+    top_p: 0.5,
+    temperature: 0.1,
+  },
+} as AgentLifeCycle;
