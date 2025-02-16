5e017b5bae0d52bc490cd5f6a578e38694f739bf
jimao <shihao.gu@perfma.com>
2025-02-16 09:52:56 +0800

    feat: XSea_执行压测

--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import XSea_Debug from "../agent/xsea/XSea_Debug";
+import XSea_执行压测 from "../agent/xsea/XSea_执行压测";
 
 const Agents = () => {
   return (
     <>
       <XSea_Debug />
+      <XSea_执行压测 />
     </>
   );
 };
