5cf191a6031fd6e23a5bdb8744ea53949fda5cae
jimao <shihao.gu@perfma.com>
2025-02-16 10:06:01 +0800

    feat: xsea智能体的加入

---
+++ app/agent/xsea/XSea_智能体.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent, { MaybeAgentSwitcher } from "..";
+import { AgentStore } from "../store";
+
+export class Agent_XSea_智能体 extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_智能体",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: `
+确保中文回答，确保回答"智能体"，避免回答非"智能体"文本，避免回答超过两个汉字，避免回答英文
+          `.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+
+  public onBeforeActive(): MaybeAgentSwitcher {
+    return {
+      agentName: "XSea_Debug",
+      bridgeMessages: [],
+    };
+  }
+}
+
+const XSea_智能体 = () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  const agent = new Agent_XSea_智能体(chatStore, navigate);
+  useEffect(() => {
+    AgentStore.register(agent.Name, agent);
+  }, []);
+  return <></>;
+};
+
+export default XSea_智能体;
--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import XSea_Debug from "../agent/xsea/XSea_Debug";
 import XSea_执行压测 from "../agent/xsea/XSea_执行压测";
+import XSea_智能体 from "../agent/xsea/XSea_智能体";
 
 const Agents = () => {
   return (
     <>
       <XSea_Debug />
+      <XSea_智能体 />
       <XSea_执行压测 />
     </>
   );
