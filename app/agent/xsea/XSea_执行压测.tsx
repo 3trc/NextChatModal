8be2c1af8a377fe58ed7013f6195a6ebed86c672
jimao <shihao.gu@perfma.com>
2025-02-16 09:52:03 +0800

    feat: XSea执行压测

---
+++ app/agent/xsea/XSea_执行压测.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent from "..";
+import { AgentStore } from "../store";
+
+export class Agent_XSea_执行压测 extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_执行压测",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: `
+确保中文回答，确保回答"开始"，避免回答非"开始"文本，避免回答超过两个汉字，避免回答英文
+          `.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+}
+
+const XSea_执行压测 = () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  const agent = new Agent_XSea_执行压测(chatStore, navigate);
+  useEffect(() => {
+    AgentStore.register(agent.Name, agent);
+  }, []);
+  return <></>;
+};
+
+export default XSea_执行压测;
