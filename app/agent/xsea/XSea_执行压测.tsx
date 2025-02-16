6e284bcf8cf4ccd88a59e0c56aea6586e6d643b9
jimao <shihao.gu@perfma.com>
2025-02-16 15:38:38 +0800

    feat: 确认压测引入

--- app/agent/xsea/XSea_执行压测.tsx
+++ app/agent/xsea/XSea_执行压测.tsx
@@ -36,15 +36,15 @@ export class Agent_XSea_执行压测 extends Agent {
 
   public onBeforeActive(): MaybeAgentSwitcher {
     if (LocalJSON.selected_scripts?.length > 0) {
-      return {
-        agentName: "XSea_确认压测",
-        bridgeMessages: [
-          {
-            role: "assistant",
-            content: "好的，这就为你准备启动压测 🚀",
-          },
-        ],
-      };
+      // return {
+      //   agentName: "XSea_确认压测",
+      //   bridgeMessages: [
+      //     {
+      //       role: "assistant",
+      //       content: "好的，这就为你准备启动压测 🚀",
+      //     },
+      //   ],
+      // };
     } else {
       return {
         agentName: "XSea_查询脚本",
@@ -66,43 +66,45 @@ export class Agent_XSea_执行压测 extends Agent {
   public RouteMap(): AgentRouteMap {
     return {
       肯定: {
-        产品: "XSea_查询产品",
-        脚本: "XSea_查询脚本",
-        计划: "XSea_查询计划",
-        压测: "XSea_查询压测",
-        记录: "XSea_查询记录",
-        概念: "XSea_查询脚本",
+        产品: "XSea_确认压测",
+        脚本: "XSea_确认压测",
+        计划: "XSea_确认压测",
+        压测: "XSea_确认压测",
+        记录: "XSea_确认压测",
+        概念: "XSea_确认压测",
+        其他: "XSea_确认压测",
       },
       否定: {
-        产品: "XSea_查询产品",
+        产品: "XSea_查询脚本",
         脚本: "XSea_查询脚本",
         计划: "XSea_查询脚本",
         压测: "XSea_查询脚本",
-        记录: "XSea_查询记录",
+        记录: "XSea_查询脚本",
         概念: "XSea_查询脚本",
         其他: "XSea_查询脚本",
       },
       终止: {
         产品: "XSea_查询产品",
         脚本: "XSea_查询脚本",
-        计划: "XSea_查询计划",
-        压测: "XSea_查询压测",
+        计划: "XSea_查询脚本",
+        压测: "XSea_智能体",
         记录: "XSea_查询记录",
         概念: "XSea_知识库",
+        其他: "XSea_智能体",
       },
       陈述: {
         产品: "XSea_知识库",
         脚本: "XSea_知识库",
         计划: "XSea_知识库",
-        压测: "XSea_知识库",
+        压测: "XSea_确认压测",
         记录: "XSea_知识库",
         概念: "XSea_知识库",
       },
       创建: {
         产品: "XSea_创建产品",
-        脚本: "XSea_创建脚本",
+        脚本: "XSea_查询脚本",
         计划: "XSea_创建计划",
-        压测: "XSea_执行压测",
+        压测: "XSea_确认压测",
         记录: "XSea_执行压测",
         概念: "XSea_知识库",
       },
@@ -117,7 +119,7 @@ export class Agent_XSea_执行压测 extends Agent {
       },
       询问: {
         产品: "XSea_知识库",
-        脚本: "XSea_知识库",
+        脚本: "XSea_查询脚本",
         计划: "XSea_知识库",
         压测: "XSea_知识库",
         记录: "XSea_知识库",
@@ -128,7 +130,7 @@ export class Agent_XSea_执行压测 extends Agent {
         产品: "XSea_查询产品",
         脚本: "XSea_查询脚本",
         计划: "XSea_查询计划",
-        压测: "XSea_查询压测",
+        压测: "XSea_查询脚本",
         记录: "XSea_查询记录",
         概念: "XSea_知识库",
         其他: "XSea_查询脚本",
@@ -137,16 +139,16 @@ export class Agent_XSea_执行压测 extends Agent {
         产品: "XSea_知识库",
         脚本: "XSea_执行脚本",
         计划: "XSea_执行压测",
-        压测: "XSea_执行压测",
-        记录: "XSea_执行压测",
+        压测: "XSea_确认压测",
+        记录: "XSea_确认压测",
         概念: "XSea_知识库",
         其他: "XSea_知识库",
       },
       其他: {
         产品: "XSea_知识库",
-        脚本: "XSea_知识库",
+        脚本: "XSea_查询脚本",
         计划: "XSea_知识库",
-        压测: "XSea_知识库",
+        压测: "XSea_确认压测",
         记录: "XSea_知识库",
         概念: "XSea_知识库",
       },
