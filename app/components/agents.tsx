b4e4a5858683a8a0fb208a75af5cb979ae31d539
jimao <shihao.gu@perfma.com>
2025-02-17 12:19:18 +0800

    feat: XSea模板

--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import XSea_Debug from "../agent/xsea/XSea_Debug";
 import XSea_执行压测 from "../agent/xsea/XSea_执行压测";
 import XSea_智能体 from "../agent/xsea/XSea_智能体";
 import XSea_查询产品 from "../agent/xsea/XSea_查询产品";
 import XSea_查询脚本 from "../agent/xsea/XSea_查询脚本";
 import XSea_知识库 from "../agent/xsea/XSea_知识库";
 import XSea_确认压测 from "../agent/xsea/XSea_确认压测";
+import XSea_模板 from "../agent/xsea/XSea_模板";
 
 const Agents = () => {
   return (
     <>
-      <XSea_Debug />
       <XSea_智能体 />
       <XSea_执行压测 />
       <XSea_查询产品 />
       <XSea_查询脚本 />
       <XSea_知识库 />
       <XSea_确认压测 />
+      <XSea_模板 />
     </>
   );
 };
