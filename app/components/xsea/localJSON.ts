e1470fdc51f94ad8f9822f7c1f50c7fd8ddcdbc3
jimao <shihao.gu@perfma.com>
2025-02-17 11:28:29 +0800

    feat: session存储

--- app/components/xsea/localJSON.ts
+++ app/components/xsea/localJSON.ts
@@ -1,17 +1,17 @@
-const handler = {
-  get: (_: any, property: any) => {
+const sessionHandler = {
+  get: (target: any, property: any) => {
     try {
       return JSON.parse(sessionStorage.getItem(property) as string);
     } catch (error) {
       return null;
     }
   },
-  set: (_: any, property: any, value: any) => {
+  set: (target: any, property: any, value: any) => {
     sessionStorage.setItem(property, JSON.stringify(value));
     return true;
   },
 };
 
-const LocalJSON = new Proxy({}, handler);
+const SessionJSON = new Proxy({}, sessionHandler);
 
 export default LocalJSON;
