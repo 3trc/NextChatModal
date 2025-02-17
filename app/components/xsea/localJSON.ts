482d19cf8e48e55c5590a9976d64093e21364106
jimao <shihao.gu@perfma.com>
2025-02-17 11:15:32 +0800

    feat: 更换存储为session

--- app/components/xsea/localJSON.ts
+++ app/components/xsea/localJSON.ts
@@ -1,13 +1,13 @@
 const handler = {
   get: (_: any, property: any) => {
     try {
-      return JSON.parse(localStorage.getItem(property) as string);
+      return JSON.parse(sessionStorage.getItem(property) as string);
     } catch (error) {
       return null;
     }
   },
   set: (_: any, property: any, value: any) => {
-    localStorage.setItem(property, JSON.stringify(value));
+    sessionStorage.setItem(property, JSON.stringify(value));
     return true;
   },
 };
