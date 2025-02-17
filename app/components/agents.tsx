059554927a353171111939ebe3546e53a63021e6
jimao <shihao.gu@perfma.com>
2025-02-17 18:18:20 +0800

    feat: 准备重写Agent

--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import XSea_执行压测 from "../agent/xsea/XSea_执行压测";
-import XSea_智能体 from "../agent/xsea/XSea_智能体";
-import XSea_查询产品 from "../agent/xsea/XSea_查询产品";
-import XSea_查询脚本 from "../agent/xsea/XSea_查询脚本";
-import XSea_知识库 from "../agent/xsea/XSea_知识库";
-import XSea_确认压测 from "../agent/xsea/XSea_确认压测";
 import XSea_模板 from "../agent/xsea/XSea_模板";
 import XSea_测试 from "../agent/xsea/XSea_测试";
 import XSea_测试_A from "../agent/xsea/XSea_测试_A";
@@ -14,12 +8,6 @@ import XSea_确认机 from "../agent/xsea/XSea_确认机";
 const Agents = () => {
   return (
     <>
-      <XSea_智能体 />
-      <XSea_执行压测 />
-      <XSea_查询产品 />
-      <XSea_查询脚本 />
-      <XSea_知识库 />
-      <XSea_确认压测 />
       <XSea_测试 />
       <XSea_测试_A />
       <XSea_测试_B />
