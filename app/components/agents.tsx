1496402ca62357b2f8149997d55dbba99440bec1
jimao <shihao.gu@perfma.com>
2025-02-17 12:24:56 +0800

    feat: 整理代码提交

--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -6,6 +6,7 @@ import XSea_查询脚本 from "../agent/xsea/XSea_查询脚本";
 import XSea_知识库 from "../agent/xsea/XSea_知识库";
 import XSea_确认压测 from "../agent/xsea/XSea_确认压测";
 import XSea_模板 from "../agent/xsea/XSea_模板";
+import XSea_测试 from "../agent/xsea/XSea_测试";
 
 const Agents = () => {
   return (
@@ -16,6 +17,7 @@ const Agents = () => {
       <XSea_查询脚本 />
       <XSea_知识库 />
       <XSea_确认压测 />
+      <XSea_测试 />
       <XSea_模板 />
     </>
   );
