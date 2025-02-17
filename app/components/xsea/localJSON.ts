fc18af134a6a3a171bf4bac27ed0528a70432107
jimao <shihao.gu@perfma.com>
2025-02-17 11:33:26 +0800

    feat: 丰富完善

--- app/components/xsea/localJSON.ts
+++ app/components/xsea/localJSON.ts
@@ -12,6 +12,22 @@ const sessionHandler = {
   },
 };
 
-const SessionJSON = new Proxy({}, sessionHandler);
+const localHandler = {
+  get: (target: any, property: any) => {
+    try {
+      return JSON.parse(localStorage.getItem(property) as string);
+    } catch (error) {
+      return null;
+    }
+  },
+  set: (target: any, property: any, value: any) => {
+    localStorage.setItem(property, JSON.stringify(value));
+    return true;
+  },
+};
+
+export const SessionJSON = new Proxy({}, sessionHandler);
+
+const LocalJSON = new Proxy({}, localHandler);
 
 export default LocalJSON;
