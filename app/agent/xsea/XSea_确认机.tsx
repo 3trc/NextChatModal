df456ff9dea239e2ea44d40593b41a9a88eb5693
jimao <shihao.gu@perfma.com>
2025-02-17 15:56:25 +0800

    feat: 准备调试确认机

---
+++ app/agent/xsea/XSea_确认机.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent, { AgentRouteMap } from "..";
+import { AgentStore } from "../store";
+
+class _Agent extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_确认机",
+        context: [],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+
+  public RouteMap(): AgentRouteMap {
+    return {
+      终止: "XSea_测试",
+    };
+  }
+}
+
+export default () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  useEffect(() => {
+    const agent = new _Agent(chatStore, navigate);
+    AgentStore.register(agent.Name, agent);
+  }, [chatStore, navigate]);
+  return <></>;
+};
--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -9,6 +9,7 @@ import XSea_模板 from "../agent/xsea/XSea_模板";
 import XSea_测试 from "../agent/xsea/XSea_测试";
 import XSea_测试_A from "../agent/xsea/XSea_测试_A";
 import XSea_测试_B from "../agent/xsea/XSea_测试_B";
+import XSea_确认机 from "../agent/xsea/XSea_确认机";
 
 const Agents = () => {
   return (
@@ -22,6 +23,7 @@ const Agents = () => {
       <XSea_测试 />
       <XSea_测试_A />
       <XSea_测试_B />
+      <XSea_确认机 />
       <XSea_模板 />
     </>
   );
