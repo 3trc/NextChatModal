9d33fc3f85963232bc7cc1945b98b3513d219027
jimao <shihao.gu@perfma.com>
2025-02-17 18:20:17 +0800

    feat: XSea智能体和摸摸鱼

--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -4,10 +4,14 @@ import XSea_测试 from "../agent/xsea/XSea_测试";
 import XSea_测试_A from "../agent/xsea/XSea_测试_A";
 import XSea_测试_B from "../agent/xsea/XSea_测试_B";
 import XSea_确认机 from "../agent/xsea/XSea_确认机";
+import XSea_智能体 from "../agent/xsea/XSea_智能体";
+import XSea_摸摸鱼 from "../agent/xsea/XSea_摸摸鱼";
 
 const Agents = () => {
   return (
     <>
+      <XSea_智能体 />
+      <XSea_摸摸鱼 />
       <XSea_测试 />
       <XSea_测试_A />
       <XSea_测试_B />
