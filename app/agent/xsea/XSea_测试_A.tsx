274ba086754a37566a3763b0a137502fd42d86ab
jimao <shihao.gu@perfma.com>
2025-02-17 12:55:44 +0800

    feat: 测试AB

---
+++ app/agent/xsea/XSea_测试_A.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent, { AgentRouteMap } from "..";
+import { AgentStore } from "../store";
+
+class _Agent extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_测试_A",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: `你是一个公主，你需要与用户对话`.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+
+  public RouteMap(): AgentRouteMap {
+    return {};
+  }
+}
+
+export default () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  useEffect(() => {
+    const agent = new _Agent(chatStore, navigate);
+    AgentStore.register(agent.Name, agent);
+  }, [chatStore, navigate]);
+  return <></>;
+};
