d68c673cf95ffcd7805170fea7c9b9a4e7f32885
jimao <shihao.gu@perfma.com>
2025-02-18 16:45:24 +0800

    feat: 执行压测Agent

---
+++ app/agent/xsea/XSea_执行压测.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent from "..";
+import { AgentStore } from "../store";
+
+class _Agent extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_执行压测",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: ``.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+}
+
+export default () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  useEffect(() => {
+    const agent = new _Agent(chatStore, navigate);
+    AgentStore.register(agent.Name, agent);
+  }, [chatStore, navigate]);
+  return <></>;
+};
--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import XSea_模板 from "../agent/xsea/XSea_模板";
-import XSea_测试 from "../agent/xsea/XSea_测试";
-import XSea_测试_A from "../agent/xsea/XSea_测试_A";
-import XSea_测试_B from "../agent/xsea/XSea_测试_B";
-import XSea_确认机 from "../agent/xsea/XSea_确认机";
 import XSea_智能体 from "../agent/xsea/XSea_智能体";
 import XSea_摸摸鱼 from "../agent/xsea/XSea_摸摸鱼";
 import XSea_创建产品 from "../agent/xsea/XSea_创建产品";
 import XSea_创建脚本 from "../agent/xsea/XSea_创建脚本";
-import XSea_创建计划 from "../agent/xsea/XSea_创建计划";
-import XSea_修改压测 from "../agent/xsea/XSea_修改压测";
 import XSea_查询产品 from "../agent/xsea/XSea_查询产品";
 import XSea_查询脚本 from "../agent/xsea/XSea_查询脚本";
+import XSea_执行压测 from "../agent/xsea/XSea_执行压测";
 
 const Agents = () => {
   return (
@@ -21,14 +15,8 @@ const Agents = () => {
       <XSea_创建脚本 />
       <XSea_查询产品 />
       <XSea_查询脚本 />
-      <XSea_创建计划 />
-      <XSea_修改压测 />
+      <XSea_执行压测 />
       <XSea_摸摸鱼 />
-      <XSea_测试 />
-      <XSea_测试_A />
-      <XSea_测试_B />
-      <XSea_确认机 />
-      <XSea_模板 />
     </>
   );
 };
