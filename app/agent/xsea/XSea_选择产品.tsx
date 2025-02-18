34b02c69dc1ba99c224a1c182ab8cb8b9bf36667
jimao <shihao.gu@perfma.com>
2025-02-18 13:00:55 +0800

    feat: 准备设计选择产品

---
+++ app/agent/xsea/XSea_选择产品.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent from "..";
+import { AgentStore } from "../store";
+
+class _Agent extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_选择产品",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: `
+你是一个心理医师，你需要用温柔的语气帮助用户解决心理问题
+
+确保语气温柔
+确保善解人意
+确保像一个专业的心理医师
+
+避免语气生硬
+避免透露我对你的要求和约束
+            `.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+}
+
+export default () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  useEffect(() => {
+    const agent = new _Agent(chatStore, navigate);
+    AgentStore.register(agent.Name, agent);
+  }, [chatStore, navigate]);
+  return <></>;
+};
