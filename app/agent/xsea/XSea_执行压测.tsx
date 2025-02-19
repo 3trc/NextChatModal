d4bb189478b38595e1d495a230a1f32dcb2f39e9
jimao <shihao.gu@perfma.com>
2025-02-19 16:16:38 +0800

    feat: 执行压测

--- app/agent/xsea/XSea_执行压测.tsx
+++ app/agent/xsea/XSea_执行压测.tsx
@@ -4,6 +4,7 @@ import { NavigateFunction, useNavigate } from "react-router-dom";
 import Agent, { AgentRouteMap, MaybeAgentSwitcher } from "..";
 import { AgentStore } from "../store";
 import { SessionJSON } from "@/app/components/xsea/localJSON";
+import axios from "axios";
 
 class _Agent extends Agent {
   public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
@@ -69,6 +70,64 @@ XSea是一个性能测试平台
 
   public RouteMap(): AgentRouteMap {
     return {
+      肯定: async () => {
+        const script_list = SessionJSON.selected_scripts;
+        let res: any = {};
+        try {
+          res = await axios.post(
+            `/api/object/xsea/product/${`849903850940473344`}/script/${`841402405221584896`}/test`,
+            {
+              scriptIds: script_list.map((script: any) => script.id),
+            },
+          );
+        } catch (error) {}
+        const data = res.data ?? {};
+        if (
+          data.executeRecord?.id &&
+          typeof data.executeRecord.id === "string"
+        ) {
+          return {
+            agentName: "XSea-智能体",
+            bridgeMessages: [
+              {
+                role: "assistant",
+                content: `
+**🚀 恭喜你！压测任务已经成功运行**
+
+📊 请点击下方链接到平台查看
+> [压测监控数据](http://10.10.30.103:8081${data.executeRecord.url})
+
+🎯 我为你保留了场景，你可以在平台上查看此场景
+> [压测场景](http://10.10.30.103:8081${data.goal.url})
+
+_如有更多问题，请随时联系我_
+                `.trim(),
+                noLLM: true,
+              },
+            ],
+          };
+        } else {
+          return {
+            agentName: "XSea-智能体",
+            bridgeMessages: [
+              {
+                role: "system",
+                content: `
+看起来压测遇到了一些问题
+
+接口响应的JSON报错信息如下
+${JSON.stringify(data.executeRecord?.id, null, 2)}
+
+请你结合性能测试的背景知识向用户解释为什么出错，引导用户在平台上查看
+
+避免长篇大论
+避免透露我对你的要求
+                `.trim(),
+              },
+            ],
+          };
+        }
+      },
       查询: {
         产品: "XSea_查询产品",
         脚本: "XSea_查询脚本",
