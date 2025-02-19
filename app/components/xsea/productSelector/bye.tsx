c52460580882eedc12deb0bf7bdc65a13f6aaaf0
jimao <shihao.gu@perfma.com>
2025-02-19 14:43:29 +0800

    feat: 尝试

--- app/components/xsea/productSelector/bye.tsx
+++ app/components/xsea/productSelector/bye.tsx
@@ -5,7 +5,7 @@ const ProductSelectorBye = () => {
   return (
     <div className="text-rows">
       <div>👍🏻 当前，你已经成功选择了一个产品</div>
-      <div>接下来你可以尝试，或者任意其他事情 😊</div>
+      <div>接下来你可以尝试以下，或者任意其他事情 😊</div>
       <div>
         <Space>
           <Button type="primary">创建脚本</Button>
