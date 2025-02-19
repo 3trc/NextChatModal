39cb0ab7ad140158588d71ccb254e78ac8db7c7f
jimao <shihao.gu@perfma.com>
2025-02-19 15:42:29 +0800

    feat: 样式优化与调整

--- app/components/xsea/stateConfirm.tsx
+++ app/components/xsea/stateConfirm.tsx
@@ -1,23 +1,47 @@
 import { Button, Space } from "antd";
 import React from "react";
+import { SessionJSON } from "./localJSON";
 
 const StateConfirm = () => {
   return (
-    <div className="text-rows">
+    <div className="text-rows" style={{ width: "300px" }}>
       <div>
-        当前我们在 <a>产品</a> 下面
+        🦄 当前我们在{" "}
+        <a
+          target="_blank"
+          href={`http://10.10.30.103:8081${SessionJSON.selected_product?.url}`}
+        >
+          {SessionJSON.selected_product?.name}
+        </a>{" "}
+        下面
       </div>
       <div>我们选择了以下脚本：</div>
       <div>
         <ul>
-          <li>123</li>
-          <li>123</li>
+          {(SessionJSON.selected_scripts ?? []).map(
+            (script: any, index: number) => (
+              <li key={script.id}>
+                <span>{`${index + 1}.`}</span>
+                <a
+                  target="_blank"
+                  href={`http://10.10.30.103:8081${script?.url}`}
+                >
+                  {script.name}
+                </a>
+              </li>
+            ),
+          )}
         </ul>
       </div>
-      <div>
+      <div>是否现在开始压测呢？🤔</div>
+      <div className="buttons">
+        <span></span>
         <Space>
+          <Button size="small" type="link">
+            更换脚本
+          </Button>
           <Button>取消</Button>
-          <Button type="primary">确认</Button>
+          <Button type="primary">压测</Button>
         </Space>
       </div>
     </div>
