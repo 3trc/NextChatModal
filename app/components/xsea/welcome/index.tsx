30de65c94a8ca3abf5a068d6405efcd00048af9a
jimao <shihao.gu@perfma.com>
2025-02-20 13:21:43 +0800

    feat: 啊你哭

--- app/components/xsea/welcome/index.tsx
+++ app/components/xsea/welcome/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./index.module.scss";
 import { Button } from "antd";
 import { useChatStore } from "@/app/store";
+import { AgentStore } from "@/app/agent/store";
 
 const Welcome = () => {
   const chatStore = useChatStore();
@@ -16,7 +17,15 @@ const Welcome = () => {
         <ul>
           <li>
             <div>
-              <Button size="small" type="primary">
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => {
+                  AgentStore.get(
+                    chatStore.currentSession().mask.name,
+                  ).SendMessage("请开始快速压测");
+                }}
+              >
                 快速压测
               </Button>
               <span>
@@ -27,7 +36,16 @@ const Welcome = () => {
           </li>
           <li>
             <div>
-              <Button size="small">创建脚本</Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  AgentStore.get(
+                    chatStore.currentSession().mask.name,
+                  ).SendMessage("请帮我创建一个脚本");
+                }}
+              >
+                创建脚本
+              </Button>
               <span>
                 📜 需要我帮助你编写一些 <b>压测脚本</b> 吗？无论是 <b>JMeter</b>{" "}
                 还是 <b>Gatling</b> 亦或是 <b>Shell</b>
@@ -37,7 +55,16 @@ const Welcome = () => {
           </li>
           <li>
             <div>
-              <Button size="small">创建产品</Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  AgentStore.get(
+                    chatStore.currentSession().mask.name,
+                  ).SendMessage("请帮我创建一个产品");
+                }}
+              >
+                创建产品
+              </Button>
               <span>
                 💡 产品是XSea之中的 <b>顶层概念</b>，用于管理 脚本 测试计划
                 等等其他性能测试相关资源。
@@ -46,7 +73,16 @@ const Welcome = () => {
           </li>
           <li>
             <div>
-              <Button size="small">查询脚本</Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  AgentStore.get(
+                    chatStore.currentSession().mask.name,
+                  ).SendMessage("请列出所有脚本");
+                }}
+              >
+                查询脚本
+              </Button>
               <span>
                 📜 需要查询平台上的 <b>已有脚本</b>{" "}
                 吗，通常我们选择了一些脚本就可以立即快速压测了哦！
@@ -55,7 +91,16 @@ const Welcome = () => {
           </li>
           <li>
             <div>
-              <Button size="small">查询产品</Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  AgentStore.get(
+                    chatStore.currentSession().mask.name,
+                  ).SendMessage("请列出所有产品");
+                }}
+              >
+                查询产品
+              </Button>
               <span>
                 💡 如果你想了解 <b>现有的产品</b>{" "}
                 信息，或者需要查找某个特定的产品，请告诉我具体的需求。
@@ -64,7 +109,16 @@ const Welcome = () => {
           </li>
           <li>
             <div>
-              <Button size="small">学习XSea性能测试平台</Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  AgentStore.get(
+                    chatStore.currentSession().mask.name,
+                  ).SendMessage("什么是XSea");
+                }}
+              >
+                学习XSea性能测试平台
+              </Button>
               <span>
                 📚 使用XSea的过程中遇到了难以解决的问题，试着与 <b>知识库</b>{" "}
                 对话，解答你的一切疑惑。
