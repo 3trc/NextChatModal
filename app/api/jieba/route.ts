56a8f0b867f0fbbaf878086bb7424f447e252dd5
jimao <shihao.gu@perfma.com>
2025-02-23 12:04:41 +0800

    feat: jieba分词准备整合知识库

--- app/api/jieba/route.ts
+++ app/api/jieba/route.ts
@@ -5,21 +5,18 @@ export async function GET(request: NextRequest) {
   try {
     const text =
       "XSea之中，所以，😄，你知道怎么安装探针吗，你这个小丑，回答我把，哈哈哈";
-    const tags = nodejieba
+    const words = nodejieba
       .tag(text)
-      .filter((tag) => ["n", "eng"].includes(tag.tag))
+      .filter((tag) =>
+        ["n", "eng", "v"].some((type) => tag.tag.startsWith(type)),
+      )
       .map((tag) => tag.word);
-    return NextResponse.json(
-      {
-        result: tags,
+    return NextResponse.json(words, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
       },
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
+    });
   } catch (error) {
     return NextResponse.json(
       {
