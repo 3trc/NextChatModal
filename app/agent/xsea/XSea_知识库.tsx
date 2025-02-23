466399e8dd86bb73cbb3675fe1d33f68657592bb
jimao <shihao.gu@perfma.com>
2025-02-23 13:06:53 +0800

    feat: RouteMap

--- app/agent/xsea/XSea_知识库.tsx
+++ app/agent/xsea/XSea_知识库.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { ChatStore, useChatStore } from "@/app/store";
 import { NavigateFunction, useNavigate } from "react-router-dom";
-import Agent from "..";
+import Agent, { AgentRouteMap } from "..";
 import { AgentStore } from "../store";
 import axios from "axios";
 
@@ -65,6 +65,27 @@ ${JSON.stringify(data, null, 2)}
         return this;
       }
     });
+  }
+
+  public RouteMap(): AgentRouteMap {
+    return {
+      查询: {
+        产品: "XSea_查询产品",
+        脚本: "XSea_查询脚本",
+      },
+      询问: {
+        产品: "XSea_查询产品",
+        脚本: "XSea_查询脚本",
+      },
+      创建: {
+        产品: "XSea_创建产品",
+        脚本: "XSea_创建脚本",
+        压测: "XSea_执行压测",
+      },
+      执行: {
+        压测: "XSea_执行压测",
+      },
+    };
   }
 }
 
