273f0bedf91a83a04120a0ad973b0a8074719a8c
jimao <shihao.gu@perfma.com>
2025-02-23 10:12:32 +0800

    feat: XSea知识库

---
+++ app/agent/xsea/XSea_知识库.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent from "..";
+import { AgentStore } from "../store";
+
+class _Agent extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XSea_知识库",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: `
+你是XSea知识库
+            `.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "perfma-gpt-14b:latest",
+          max_tokens: 16384,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+}
+
+export default () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  useEffect(() => {
+    const agent = new _Agent(chatStore, navigate);
+    AgentStore.register(agent.Name, agent);
+  }, [chatStore, navigate]);
+  return <></>;
+};
--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -8,6 +8,7 @@ import XSea_查询脚本 from "../agent/xsea/XSea_查询脚本";
 import XSea_执行压测 from "../agent/xsea/XSea_执行压测";
 import XSea_调用栈分析 from "../agent/xsea/XSea_调用栈分析";
 import { AgentStore } from "../agent/store";
+import XSea_知识库 from "../agent/xsea/XSea_知识库";
 
 const Agents = () => {
   const receiveMessage = (data: any) => {
@@ -52,6 +53,7 @@ const Agents = () => {
       <XSea_查询脚本 />
       <XSea_执行压测 />
       <XSea_调用栈分析 />
+      <XSea_知识库 />
       <XSea_摸摸鱼 />
     </>
   );
