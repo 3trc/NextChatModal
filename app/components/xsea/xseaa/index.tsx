4fc133bec9e438fb3cffc132018b4ad5c048c591
jimao <shihao.gu@perfma.com>
2025-02-25 21:38:58 +0800

    feat: 继续完善组件

--- app/components/xsea/xseaa/index.tsx
+++ app/components/xsea/xseaa/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 export interface XSeaObject {
-  type: "PRODUCT" | "SCRIPT" | "PLAN" | "GOAL" | "RECORD" | "REPORT";
+  type:
+    | "PRODUCT"
+    | "SCRIPT"
+    | "PLAN"
+    | "GOAL"
+    | "RECORD"
+    | "REPORT"
+    | "SCHEDULE";
   productId: string;
   productName: string;
   scriptId?: string;
@@ -12,7 +19,34 @@ export interface XSeaObject {
   goalName?: string;
 }
 
-const XSeaA = (props: { data: any }) => {
+export type XSeaObjectTypeMap<T> = {
+  [key in XSeaObject["type"]]: T;
+};
+
+export const EmojiMap: XSeaObjectTypeMap<string> = {
+  PRODUCT: "🧰",
+  SCRIPT: "📝",
+  PLAN: "📅",
+  GOAL: "🎯",
+  RECORD: "📋",
+  REPORT: "📊",
+  SCHEDULE: "⏰",
+};
+
+const XSeaA = (props: { data: XSeaObject }) => {
+  const { data } = props;
+  const name = useMemo(() => {
+    const nameMap: XSeaObjectTypeMap<string | undefined> = {
+      PRODUCT: data.productName,
+      SCRIPT: data.scriptName,
+      PLAN: data.planName,
+      GOAL: data.goalName,
+      RECORD: undefined,
+      REPORT: undefined,
+      SCHEDULE: undefined,
+    };
+    return nameMap[data.type];
+  }, [data]);
   return <a>{}</a>;
 };
 
