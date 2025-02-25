a0b41d46175ddacb7b6a961a67abe30ad4427b47
jimao <shihao.gu@perfma.com>
2025-02-25 17:59:03 +0800

    feat: 调整打分

--- app/api/object/xsea/global/route.ts
+++ app/api/object/xsea/global/route.ts
@@ -54,14 +54,11 @@ export async function GET(request: NextRequest) {
       ...item,
     }));
     const allList = [...scriptList, ...goalList].map((item) => {
-      const allValueText = Object.values({
-        ...item,
-        type: null,
-      })
-        .map((value) =>
-          value == null ? "" : value.toString().trim().toLowerCase(),
-        )
-        .filter((value) => value)
+      const allValueText = Object.keys(item)
+        .filter((key) => key.toLowerCase().includes("name"))
+        .map((key) => item[key])
+        .filter((value) => value != null)
+        .map((value) => value.toString().trim().toLowerCase())
         .join(",");
       const score = words.filter((word) => allValueText.includes(word)).length;
       return { ...item, score };
