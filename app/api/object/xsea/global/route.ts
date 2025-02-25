63d6a967b28225c7ed456af55525abd8281cba4f
jimao <shihao.gu@perfma.com>
2025-02-25 14:50:49 +0800

    feat: 新的接口

---
+++ app/api/object/xsea/global/route.ts
@@ -0,0 +1,38 @@
+import http from "@/app/api/simplifier/http";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const [scriptRes, goalRes] = await Promise.all([
+      http.post(`http://10.10.30.103:8081/api/xsea/script/queryScriptRel`),
+      http.post(`http://10.10.30.103:8081/api/xsea/plan/goal/queryGoalRel`),
+    ]);
+    const scriptList: any[] = (scriptRes.data.object ?? []).map(
+      (item: any) => ({
+        type: "SCRIPT",
+        ...item,
+      }),
+    );
+    const goalList: any[] = (goalRes.data.object ?? []).map((item: any) => ({
+      type: "GOAL",
+      ...item,
+    }));
+
+    return NextResponse.json(scriptList.concat(goalList), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
