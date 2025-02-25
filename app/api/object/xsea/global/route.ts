f7ce0f60fe45701a4b54763352e09a082cd0772f
jimao <shihao.gu@perfma.com>
2025-02-25 15:05:57 +0800

    feat: 过滤表完善

--- app/api/object/xsea/global/route.ts
+++ app/api/object/xsea/global/route.ts
@@ -1,10 +1,44 @@
 import http from "@/app/api/simplifier/http";
 import { NextRequest, NextResponse } from "next/server";
+import nodejieba from "nodejieba";
 
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get("query") ?? "";
+    const tokens = nodejieba.tag(query);
+    const words = tokens
+      .filter(
+        (tag) =>
+          ![
+            "uj", // 助词
+            "f", // 方位词
+            "x", // 标点符号
+            "r", // 代词
+            "v", // 动词
+            "o", // 拟声词
+            "y", // 语气词
+            "c", // 连词
+            "p", // 介词
+            "u", // 助词
+            "xc", // 其他虚词
+            "w", // 标点符号
+            "d", // 副词
+            // "m",   // 数词
+            // "q",   // 量词
+            // "t",   // 时间词
+            // "tg",  // 时语素
+            "e", // 叹词
+            "z", // 状态词
+            "ul", // 助词
+            // "mg",  // 数语素
+            "ud", // 结构助词
+            // "ug",  // 时态助词
+            "uv", // 动词后缀
+            "uz", // 状态词尾
+          ].includes(tag.tag),
+      )
+      .map((tag) => tag.word);
     const [scriptRes, goalRes] = await Promise.all([
       http.post(`http://10.10.30.103:8081/api/xsea/script/queryScriptRel`),
       http.post(`http://10.10.30.103:8081/api/xsea/plan/goal/queryGoalRel`),
@@ -23,6 +57,8 @@ export async function GET(request: NextRequest) {
     return NextResponse.json(
       {
         query,
+        tokens,
+        words,
         list: scriptList.concat(goalList),
       },
       {
