eeaf7f878c4ce445664405fe0365bb8c07feb689
jimao <shihao.gu@perfma.com>
2025-02-26 15:23:52 +0800

    feat: baseurl更换

--- app/api/simplifier/http.ts
+++ app/api/simplifier/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://192.168.8.139:8080/api",
+  baseURL: "http://10.10.30.103:8081/api",
   headers: {
     cookie: "sys_token=8df3da30323244bb8db788b437d50b3a",
   },
