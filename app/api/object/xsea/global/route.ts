3af87a37784d1d76d71529310e9b929e1dcf68e3
jimao <shihao.gu@perfma.com>
2025-02-27 13:34:41 +0800

    feat: 精确的

--- app/api/object/xsea/global/route.ts
+++ app/api/object/xsea/global/route.ts
@@ -39,7 +39,8 @@ export const querySearch = async (query: string, limit = 1000) => {
     .filter((item) => item.score > 0);
   allList.sort((a, b) => b.score - a.score);
   allList.splice(limit, Infinity);
-  return { list: allList, words };
+  const precise = allList.length === 1 || allList[1].score < allList[0].score;
+  return { precise, list: allList, words };
 };
 
 export async function GET(request: NextRequest) {
