fc06d9e3aa78295c92d87177d5183668006377af
jimao <shihao.gu@perfma.com>
2025-02-27 14:10:54 +0800

    feat: 去掉mock的

--- app/agent/index.ts
+++ app/agent/index.ts
@@ -147,82 +147,82 @@ export default class Agent {
 
       const switcher = await this.onBeforeSendMessage(message);
 
-      if (message.toLowerCase().includes("下的")) {
-        let flag = false;
-        if (message.includes("东")) {
-          if (message.includes("登录")) {
-            SessionJSON.selected_product = {
-              id: "849903850940473344",
-              name: "东航-探针采集数据场景验证",
-              url: "/822313712173449216/product/business/849903850940473344/overview?tab=0",
-            };
-            SessionJSON.selected_scripts = [
-              {
-                id: "841406834943479808",
-                name: "登录接口压测",
-                type: "JMETER",
-                url: "/822313712173449216/product/business/849903850940473344/script?scriptId=841406834943479808",
-              },
-            ];
-            flag = true;
-          } else {
-            SessionJSON.selected_product = {
-              id: "849903850940473344",
-              name: "东航-探针采集数据场景验证",
-              url: "/822313712173449216/product/business/849903850940473344/overview?tab=0",
-            };
-            SessionJSON.selected_scripts = [
-              {
-                id: "724647819989913600",
-                name: "获取航班信息",
-                type: "JMETER",
-                url: "/822313712173449216/product/business/849903850940473344/script?scriptId=724647819989913600",
-              },
-            ];
-            flag = true;
-          }
-        }
-        if (message.includes("移")) {
-          SessionJSON.selected_product = {
-            id: "905025758354444288",
-            name: "中移联调使用产品",
-            url: "/822313712173449216/product/business/905025758354444288/overview?tab=0",
-          };
-          SessionJSON.selected_scripts = [
-            {
-              id: "783639651470839810",
-              name: "zySceneId-93",
-              type: "JMETER",
-              url: "/822313712173449216/product/business/905025758354444288/script?scriptId=783639651470839810",
-            },
-          ];
-          flag = true;
-        }
+      // if (message.toLowerCase().includes("下的")) {
+      //   let flag = false;
+      //   if (message.includes("东")) {
+      //     if (message.includes("登录")) {
+      //       SessionJSON.selected_product = {
+      //         id: "849903850940473344",
+      //         name: "东航-探针采集数据场景验证",
+      //         url: "/822313712173449216/product/business/849903850940473344/overview?tab=0",
+      //       };
+      //       SessionJSON.selected_scripts = [
+      //         {
+      //           id: "841406834943479808",
+      //           name: "登录接口压测",
+      //           type: "JMETER",
+      //           url: "/822313712173449216/product/business/849903850940473344/script?scriptId=841406834943479808",
+      //         },
+      //       ];
+      //       flag = true;
+      //     } else {
+      //       SessionJSON.selected_product = {
+      //         id: "849903850940473344",
+      //         name: "东航-探针采集数据场景验证",
+      //         url: "/822313712173449216/product/business/849903850940473344/overview?tab=0",
+      //       };
+      //       SessionJSON.selected_scripts = [
+      //         {
+      //           id: "724647819989913600",
+      //           name: "获取航班信息",
+      //           type: "JMETER",
+      //           url: "/822313712173449216/product/business/849903850940473344/script?scriptId=724647819989913600",
+      //         },
+      //       ];
+      //       flag = true;
+      //     }
+      //   }
+      //   if (message.includes("移")) {
+      //     SessionJSON.selected_product = {
+      //       id: "905025758354444288",
+      //       name: "中移联调使用产品",
+      //       url: "/822313712173449216/product/business/905025758354444288/overview?tab=0",
+      //     };
+      //     SessionJSON.selected_scripts = [
+      //       {
+      //         id: "783639651470839810",
+      //         name: "zySceneId-93",
+      //         type: "JMETER",
+      //         url: "/822313712173449216/product/business/905025758354444288/script?scriptId=783639651470839810",
+      //       },
+      //     ];
+      //     flag = true;
+      //   }
 
-        if (flag) {
-          // 临时加上Loading
-          await new Promise<void>((resolve) => {
-            setTimeout(() => {
-              resolve();
-            }, 1000);
-          });
-          this.chatStore.AppendRoleMessageList(
-            [
-              {
-                role: "user",
-                content: message,
-              },
-              {
-                role: "assistant",
-                content: "",
-                component: "@ui-ScriptSelectorBye",
-              },
-            ],
-            false,
-          );
-          return this;
-        }
-      }
+      //   if (flag) {
+      //     // 临时加上Loading
+      //     await new Promise<void>((resolve) => {
+      //       setTimeout(() => {
+      //         resolve();
+      //       }, 1000);
+      //     });
+      //     this.chatStore.AppendRoleMessageList(
+      //       [
+      //         {
+      //           role: "user",
+      //           content: message,
+      //         },
+      //         {
+      //           role: "assistant",
+      //           content: "",
+      //           component: "@ui-ScriptSelectorBye",
+      //         },
+      //       ],
+      //       false,
+      //     );
+      //     return this;
+      //   }
+      // }
 
       if (switcher) {
         const nextAgent = await this.SwitchAgent(switcher, {
