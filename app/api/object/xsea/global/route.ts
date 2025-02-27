cf9c846e7818a32a91c16a68e66ec9f4fe62feb7
jimao <shihao.gu@perfma.com>
2025-02-27 14:03:49 +0800

    feat: 精确字段需要外层处理

--- app/api/object/xsea/global/route.ts
+++ app/api/object/xsea/global/route.ts
@@ -39,8 +39,7 @@ export const querySearch = async (query: string, limit = 1000) => {
     .filter((item) => item.score > 0);
   allList.sort((a, b) => b.score - a.score);
   allList.splice(limit, Infinity);
-  const precise = allList.length === 1 || allList[1]?.score < allList[0]?.score;
-  return { precise, list: allList, words };
+  return { list: allList, words };
 };
 
 export async function GET(request: NextRequest) {
