e1498e3454f8ba2b45a277faaf7278163479045f
jimao <shihao.gu@perfma.com>
2025-02-27 20:53:07 +0800

    feat: 调整

--- app/api/object/xsea/global/route.ts
+++ app/api/object/xsea/global/route.ts
@@ -3,21 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import nodejieba from "nodejieba";
 
 export const querySearch = async (query: string, limit = 50) => {
-  const words = nodejieba.extract(query, 5).filter((word) => word.weight >= 4);
-  // .filter(
-  //   (word) =>
-  //     ![
-  //       "产品",
-  //       "脚本",
-  //       "计划",
-  //       "目标",
-  //       "压测",
-  //       "执行",
-  //       "报告",
-  //       "记录",
-  //       "定时",
-  //     ].includes(word.word),
-  // );
+  const words = nodejieba.extract(query, 10);
   const [scriptRes, goalRes] = await Promise.all([
     http.post(`http://10.10.30.103:8081/api/xsea/script/queryScriptRel`),
     http.post(`http://10.10.30.103:8081/api/xsea/plan/goal/queryGoalRel`),
