748d035c3be12e8ef46f4d0c1c893172a1447c41
jimao <shihao.gu@perfma.com>
2025-02-28 11:09:41 +0800

    feat: 尝试进行关键字对齐

--- app/api/object/xsea/global/route.ts
+++ app/api/object/xsea/global/route.ts
@@ -15,6 +15,7 @@ export const querySearch = async (querys: string[], limit = 50) => {
         (word) => !["压测", "执行", "调试", "开始"].includes(word.word),
       ),
     );
+  // 尝试进行关键字对齐
   const maxWordsLength = Math.max(...wordsList.map((words) => words.length));
   wordsList.forEach((words, index) => {
     wordsList[index] = Array(10).fill(words).flat().slice(0, maxWordsLength);
