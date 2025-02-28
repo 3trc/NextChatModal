7a573703443a80dd02035501d6c1cce4fee19b7d
jimao <shihao.gu@perfma.com>
2025-02-28 19:54:38 +0800

    feat: 确保语气专业

--- app/agent/index.ts
+++ app/agent/index.ts
@@ -310,7 +310,7 @@ export default class Agent {
 请询问用户是否压测 ${JSON.stringify(target, null, 2)} 脚本
 确保直接询问用户是否确认
 确保加粗名称信息
-确保以专业语气回答用户
+确保语气专业
 避免透露JSON格式的内容
 避免透露寻找过程
 避免确认其他信息
@@ -329,7 +329,7 @@ export default class Agent {
 请询问用户是否压测 ${JSON.stringify(target, null, 2)} 目标
 确保直接询问用户是否确认
 确保以markdown回答
-确保以专业语气回答用户
+确保语气专业
 避免透露JSON格式的内容
 避免透露寻找过程
 避免确认其他信息
