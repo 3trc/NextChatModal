9c76addd54188027334c724378ec97cb64d23da5
jimao <shihao.gu@perfma.com>
2025-02-28 14:20:23 +0800

    feat: 样式调整

--- app/components/xsea/targets/index.tsx
+++ app/components/xsea/targets/index.tsx
@@ -13,40 +13,49 @@ const Targets = () => {
 
   return (
     <div className={styles.com}>
-      {list.length === 0 && (
-        <div>好像没找到相关内容呢，尝试描述清楚一些？😊</div>
-      )}
-      {list.length > 0 && (
-        <div className={styles.table}>
-          <Table
-            rowKey={(row) => {
-              if (row.type === "PRODUCT") return row.productId;
-              if (row.type === "SCRIPT") return row.scriptId ?? "";
-              if (row.type === "PLAN") return row.planId ?? "";
-              if (row.type === "GOAL") return row.goalId ?? "";
-              return "";
-            }}
-            size="small"
-            showHeader={false}
-            columns={[
-              {
-                render: (row) => {
-                  return <XSeaA data={row} emoji />;
+      <div>
+        {list.length === 0 && (
+          <div>好像没找到相关内容呢，尝试描述清楚一些？😊</div>
+        )}
+        {list.length > 0 && (
+          <div className={styles.table}>
+            <Table
+              rowKey={(row) => {
+                if (row.type === "PRODUCT") return row.productId;
+                if (row.type === "SCRIPT") return row.scriptId ?? "";
+                if (row.type === "PLAN") return row.planId ?? "";
+                if (row.type === "GOAL") return row.goalId ?? "";
+                return "";
+              }}
+              size="small"
+              showHeader={false}
+              columns={[
+                {
+                  render: (row) => {
+                    return <XSeaA data={row} emoji />;
+                  },
                 },
-              },
-            ]}
-            dataSource={list}
-            pagination={
-              list.length <= 5
-                ? false
-                : {
-                    pageSize: 5,
-                    showSizeChanger: false,
-                  }
-            }
-          />
-        </div>
-      )}
+              ]}
+              dataSource={list}
+              pagination={
+                list.length <= 5
+                  ? false
+                  : {
+                      pageSize: 5,
+                      showSizeChanger: false,
+                    }
+              }
+            />
+          </div>
+        )}
+      </div>
+      <div className={styles.recommend}>
+        <ul className={styles.next_list}>
+          <li>解释一下83脚本</li>
+          <li>鸡毛目标是干什么的</li>
+          <li>帮我重新编写一个JMeter脚本吧</li>
+        </ul>
+      </div>
     </div>
   );
 };
