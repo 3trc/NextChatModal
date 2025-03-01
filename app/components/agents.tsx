e76b74a7083e1d98a0bfd10ebaf99c107482bd5d
jimao <shihao.gu@perfma.com>
2025-03-01 16:16:48 +0800

    feat: 加入XWind分析

--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -16,6 +16,7 @@ import XSea_Shell专家 from "../agent/xsea/XSea_Shell专家";
 import { SessionJSON } from "./xsea/localJSON";
 import XSea_内存分析 from "../agent/xsea/XSea_内存分析";
 import XSky_分析 from "../agent/xsea/XSky_分析";
+import XWind_分析 from "../agent/xsea/XWind_分析";
 
 const Agents = () => {
   const receiveMessage = (data: any) => {
@@ -118,6 +119,7 @@ const Agents = () => {
       <XSea_当下引导 />
       <XSea_知识库 />
       <XSky_分析 />
+      <XWind_分析 />
       <XSea_摸摸鱼 />
     </>
   );
