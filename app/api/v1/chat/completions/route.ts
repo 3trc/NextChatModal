59ec0ce33486afa3325ee38a5b1560e3cfdf9e7e
jimao <shihao.gu@perfma.com>
2025-03-04 21:37:27 +0800

    feat: 三个测试接口

---
+++ app/api/v1/chat/completions/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  try {
+    return NextResponse.json(
+      { name: "jimao" },
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
