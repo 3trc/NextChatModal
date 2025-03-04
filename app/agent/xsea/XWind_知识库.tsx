5a20904d34eff6384cf6e5f39906fe6248361d10
jimao <shihao.gu@perfma.com>
2025-03-04 10:36:19 +0800

    feat: XWind知识库

---
+++ app/agent/xsea/XWind_知识库.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { ChatStore, useChatStore } from "@/app/store";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import Agent from "..";
+import { AgentStore } from "../store";
+
+class _Agent extends Agent {
+  public constructor(chatStore: ChatStore, navigate: NavigateFunction) {
+    super(
+      {
+        avatar: "🔄",
+        name: "XWind_知识库",
+        context: [
+          {
+            id: "",
+            role: "system",
+            content: `
+            你是一个猫咪爱好者
+            `.trim(),
+            date: "",
+          },
+        ],
+        modelConfig: {
+          model: "qwen/qwen-2-72b-instruct",
+          max_tokens: 12000,
+          topK: 1,
+          top_p: 0.5,
+          temperature: 0.1,
+        },
+      },
+      chatStore,
+      navigate,
+    );
+  }
+}
+
+export default () => {
+  const navigate = useNavigate();
+  const chatStore = useChatStore();
+  useEffect(() => {
+    const agent = new _Agent(chatStore, navigate);
+    AgentStore.register(agent.Name, agent);
+  }, [chatStore, navigate]);
+  return <></>;
+};
--- app/components/agents.tsx
+++ app/components/agents.tsx
@@ -17,6 +17,7 @@ import { SessionJSON } from "./xsea/localJSON";
 import XSea_内存分析 from "../agent/xsea/XSea_内存分析";
 import XSky_分析 from "../agent/xsea/XSky_分析";
 import XWind_分析 from "../agent/xsea/XWind_分析";
+import XWind_知识库 from "../agent/xsea/XWind_知识库";
 
 const Agents = () => {
   const receiveMessage = (data: any) => {
@@ -118,6 +119,7 @@ const Agents = () => {
       <XSea_内存分析 />
       <XSea_当下引导 />
       <XSea_知识库 />
+      <XWind_知识库 />
       <XSky_分析 />
       <XWind_分析 />
       <XSea_摸摸鱼 />
