c119690de473ab1d6569dc2e618a851941ffd453
jimao <shihao.gu@perfma.com>
2025-03-06 15:13:43 +0800

    feat: provider

--- app/api/agent/xsea/model.ts
+++ app/api/agent/xsea/model.ts
@@ -1,4 +1,21 @@
 import { ChatOllama } from "@langchain/ollama";
+import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
+import { CoreMessage, generateText } from "ai";
+
+const openrouter = createOpenAICompatible({
+  name: "psbc-qwen2",
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: "http://20.200.110.20:8011/v1",
+});
+
+const openrouterModel = openrouter("qwen2-72b-int4");
+
+export const openrouterGenerate = async (messages: CoreMessage[]) => {
+  return await generateText({
+    model: openrouterModel,
+    messages: messages,
+  });
+};
 
 const model = new ChatOllama({
   baseUrl: "http://20.200.110.20:8011",
