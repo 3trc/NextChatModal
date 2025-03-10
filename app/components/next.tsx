dd6f82dbeb3603905ef01c763d98cfe116f319f5
jimao <shihao.gu@perfma.com>
2025-03-10 20:04:04 +0800

    feat: 提交

--- app/components/next.tsx
+++ app/components/next.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./next.module.scss";
+import { useChatStore } from "../store";
 
 const Next = () => {
+  const chatStore = useChatStore();
+
+  useEffect(() => {
+    const messages = chatStore.currentSession().messages.slice(10).map((item) => ({
+      role: item.role, content: item.content,
+    }));
+    console.log(messages);
+  }, []);
+
   return <ul className={styles.com}>
     <li>压测一下XXX脚本</li>
     <li>xx脚本是干什么的</li>
