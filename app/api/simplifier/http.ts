bd6ef5adbf96df6d76d6bbb4d8360a5924662d86
jimao <shihao.gu@perfma.com>
2025-03-11 01:06:31 +0800

    feat: token

--- app/api/simplifier/http.ts
+++ app/api/simplifier/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL: "http://10.10.30.103:8081/api",
   headers: {
-    cookie: "sys_token=8df3da30323244bb8db788b437d50b3a",
+    cookie: "sys_token=f58dca2b8efa4c83b807ccb14fa0de9d",
   },
 });
 
