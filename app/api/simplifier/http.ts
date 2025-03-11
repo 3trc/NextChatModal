dafe46d7f1f9a22287082b2e5b12ba87a5ad3ded
jimao <shihao.gu@perfma.com>
2025-03-11 15:55:41 +0800

    feat: 永久登录

--- app/api/simplifier/http.ts
+++ app/api/simplifier/http.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 const http = axios.create({
   baseURL: "http://10.10.30.103:8081/api",
   headers: {
-    cookie: "sys_token=f58dca2b8efa4c83b807ccb14fa0de9d",
+    // cookie: "sys_token=f58dca2b8efa4c83b807ccb14fa0de9d",
+    AccessKey: '8508f519a2544277a4f17862e5950dfb',
+    AccessKeySecret: '6fe076d4dec0446784d03cd5e54a7d64',
   },
 });
 
