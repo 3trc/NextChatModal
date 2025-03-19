10ceb4f32ac32136035cac42c87083931e259f1d
jimao <shihao.gu@perfma.com>
2025-03-19 10:47:08 +0800

    feat: env

--- app/api/simplifier/http.ts
+++ app/api/simplifier/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL: process.env.XSEA_URL + '/api',
   headers: {
-    cookie: "sys_token=f58dca2b8efa4c83b807ccb14fa0de9d",
+    cookie: process.env.XSEA_AUTH,
   },
 });
 
